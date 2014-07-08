4be47130c54dd99c5e848a2b99a75964a380b090
Dchuk <chukotski@gmail.com>
2014-07-09 00:13:57 +0300

    plot3 done

---
+++ plot3.R
@@ -0,0 +1,21 @@
+data <- read.table("./household_power_consumption.txt", sep = ";", na.strings = "?",
+                colClasses = c("character", "character", "numeric", "numeric", "numeric",
+                               "numeric", "numeric", "numeric", "numeric"),
+                               skip = 66637, nrows = 2880)
+names(data) <- c("date", "time", "globalactivepower", "globalreactivepower", "voltage",
+                 "globalintensity", "submetering1", "submetering2", "submetering3")
+
+
+data$datetime <- paste(data$date, data$time)
+data$date <- as.Date(data$date, format = "%d/%m/%Y")
+data$time <- strptime(data$time, "%H:%M:%S")
+data$datetime <- strptime(data$datetime, "%d/%m/%Y %H:%M:%S")
+
+plot(data$datetime, data$submetering1, ylab = "Energy sub metering", xlab = "", type = "n")
+lines(data$datetime, data$submetering1, col = "black")
+lines(data$datetime, data$submetering2, col = "red")
+lines(data$datetime, data$submetering3, col = "blue")
+legend("topright", lty = 1, lwd = 1, y.intersp = 0.5, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.copy(png, file = "plot3.png")
+dev.off()
